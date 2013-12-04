4b6f9c9e7d42d73c4feb2bedf340f47fa809aff3
litodam <pablo.damiani@southworks.net>
2013-12-04 18:44:19 -0300

    added gitHubIssues migrator

---
+++ GitHubIssuesMigrator/GitHubIssuesMigrator/OctokitExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace GitHubIssuesMigrator
+{
+    using System.Linq;
+    using Octokit;
+
+    public static class OctokitExtensions
+    {
+        public static NewIssue ToNewIssue(this Issue issue)
+        {
+            var newIssue = new NewIssue(issue.Title);
+            newIssue.Assignee = issue.Assignee != null ? issue.Assignee.Name : null;
+            newIssue.Body = issue.Body;
+            issue.Labels.Select(i => i.Name).ToList().ForEach(n => newIssue.Labels.Add(n));
+
+            return newIssue;
+        }
+
+        public static IssueUpdate ToIssueUpdate(this Issue issue)
+        {
+            var updatedIssue = new IssueUpdate();
+            updatedIssue.Title = issue.Title;
+            updatedIssue.Assignee = issue.Assignee != null ? issue.Assignee.Name : null;
+            updatedIssue.Body = issue.Body;
+            issue.Labels.Select(i => i.Name).ToList().ForEach(n => updatedIssue.Labels.Add(n));
+            updatedIssue.State = issue.State;
+            if (issue.Milestone != null)
+            {
+                updatedIssue.Milestone = issue.Milestone.Number;
+            }
+
+            return updatedIssue;
+        }
+    }
+}
---
+++ GitHubIssuesMigrator/GitHubIssuesMigrator/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace GitHubIssuesMigrator
+{
+    using System;
+    using System.Net.Http.Headers;
+    using Octokit;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var org = "WindowsAzure";
+            var sourceRepo = "wacom-portal";
+            var destRepo = "marker";
+            var accessToken = "token-here";
+
+            var githubClient = new GitHubClient(new ProductHeaderValue("IssuesMigrator"));
+            githubClient.Credentials = new Credentials(accessToken);
+
+            var issueRequest = new RepositoryIssueRequest();
+            issueRequest.Labels.Add("marker");
+            issueRequest.State = ItemState.Open;
+
+            var issues = githubClient.Issue.GetForRepository(org, sourceRepo, issueRequest).Result;
+
+            foreach (var issue in issues)
+            {
+                // save to new repo
+                var resNewIssue = githubClient.Issue.Create(org, destRepo, issue.ToNewIssue()).Result;
+
+                // close in source repo                
+                var updatedIssue = issue.ToIssueUpdate();
+                updatedIssue.State = ItemState.Closed;
+                var resIssueComment = githubClient.Issue.Comment.Create(org, sourceRepo, issue.Number, string.Format("{{ \"body\": \"Moved to {0} repo.\" }}", destRepo)).Result;
+                var resUpdatedIssue = githubClient.Issue.Update(org, sourceRepo, issue.Number, updatedIssue).Result;
+            }
+
+            Console.WriteLine(string.Format("Migrated {0} issues from {1} to {2}", issues.Count, sourceRepo, destRepo));
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
